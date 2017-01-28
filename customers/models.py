7288f773551db062cbb450c80d4c154ba0b12598
Carlos <carlosborja93@gmail.com>
2017-01-28 19:45:12 +0100

    Restricción de unicidad en el campo de CIF

--- customers/models.py
+++ customers/models.py
@@ -5,7 +5,7 @@ class Customer(models.Model):
     # Attributes
     address = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    id_number = models.CharField(max_length=255)
+    id_number = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=255)
     province = models.CharField(max_length=255)
